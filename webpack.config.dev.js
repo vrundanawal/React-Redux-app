621b04f84cd8f7cc482db3ee38a308e505716d03
vrundanawal <nawal.vrunda@gmail.com>
2022-11-09 14:42:48 +0000

    webpack: core config settings

---
+++ webpack.config.dev.js
@@ -0,0 +1,17 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+};
