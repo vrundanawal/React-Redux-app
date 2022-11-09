64b35d8ba725cd5167bd1aeb90dcfbbc1c660b42
vrundanawal <nawal.vrunda@gmail.com>
2022-11-09 14:48:15 +0000

    Webpack: Plugin and Module with rule, Loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,4 +22,23 @@ module.exports = {
     headers: { "Access-Control-Allow-Origin": "*" },
     https: false,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
