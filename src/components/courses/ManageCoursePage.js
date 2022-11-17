4762a692d33d8d70a9d801a66faa4ee85b5e9415
vrundanawal <nawal.vrunda@gmail.com>
2022-11-17 13:23:44 +0000

    Convert ManageCoursePage from  class component to functional component using hooks

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,43 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-//import { bindActionCreators } from "redux";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    //destructuring
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  //destructuring
+  // const { courses, authors, loadCourses, loadAuthors }  = props;
+  useEffect(() => {
     if (courses.length === 0) {
       //for loading the courses
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
     }
-
     if (authors.length === 0) {
       //for loading the authors
       loadAuthors().catch((error) => {
         alert("Loading authors failed" + error);
       });
     }
-  }
-
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }, []);
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
+
 //Proptypes
 ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  //createCourse: PropTypes.func.isRequired,
-  actions: PropTypes.object.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
 };
@@ -48,17 +42,6 @@ function mapStateToProps(state) {
     authors: state.authors,
   };
 }
-
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     //createCourse: (course) => dispatch(courseActions.createCourse(course)),
-//     // actions: bindActionCreators(courseActions, dispatch), //mapDispatchToProps with bindActionCreators
-//     actions: {
-//       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-//       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-//     },
-//   };
-// }
 
 //Implementing Object form of mapDispatchToProps
 const mapDispatchToProps = {
